25476261ac282719f196a0f0c962783c24490874
zbigg <zbigg@zbigg.org>
2007-11-26 23:12:22 +0000

    boo

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 
 
-CXXFLAGS=-I. -g
-LIBS=-lexpat
+PFLAGS=-pg -ftest-coverage -fprofile-arcs
+CXXFLAGS=-I. -g $(PFLAGS) -I/c/Programs/PostgreSQL_82/include 
+LIBS=-lexpat -L/c/Programs/PostgreSQL_82/lib -lpq
 all: taskmonitor
 
 taskmonitor: tinfra/Symbol.o taskmonitor.o
-	$(CXX) -g -o $@ $^ $(LIBS)
+	$(CXX) -g $(PFLAGS) -o $@ $^ $(LIBS)
 
 clean:
-	rm -rf taskmonitor.exe *.o
+	rm -rf taskmonitor.exe *.o tinfra/*.o
 
 taskmonitor.o: taskmonitor.cpp tinfra/tinfra.h tinfra/tinfra_lex.h tinfra/Symbol.h tinfra/XMLPrinter.h
 	$(CXX) $(CXXFLAGS) -c -o $@ $<
---
+++ postgresql.h
@@ -0,0 +1,92 @@
+#ifndef __tinfra_postgres_h__
+#define __tinfra_postgres_h__
+
+#include <iterator>
+
+#include <tinfra/tinfra.h>
+
+#include <libpq-fe.h>
+namespace tinfra { namespace postgres {
+
+/*
+    template
+*/
+
+typedef std::vector<tinfra::Symbol> symbol_vector;
+typedef std::map<tinfra::Symbol, std::string> result_map;
+
+namespace detail {
+    void get_symbols(PGresult const* result, symbol_vector& dest)
+    {
+            
+            int n_fields = PQnfields(result);
+            dest.reserve(n_fields);
+            for(int i  = 0; i < n_fields; ++i ) {
+                    char* column_name = PQfname(result,i);
+                    Symbol column = Symbol::get(column_name);
+                    dest.push_back(column);
+            }
+    }
+}
+
+typedef std::vector<tinfra::Symbol> symbol_vector;
+template <typename T>
+        void fetch_row(const PGresult* result, int row_number, symbol_vector const& symbols, T& dest)
+        {
+                int ic = 0;
+                for( symbol_vector::const_iterator is = symbols.begin();
+                     is != symbols.end();
+                     ++is,++ic)
+                {
+                        if( PQgetisnull(result,row_number, ic) ) continue;
+                        char* value = PQgetvalue(result, row_number, ic);
+                        tinfra::lexical_set<T>(dest, *is, value);
+                }
+        }    
+    
+void fetch_row(const PGresult* result, int row_number, symbol_vector const& symbols, result_map& dest)
+{
+        int ic = 0;
+        for( symbol_vector::const_iterator is = symbols.begin();
+             is != symbols.end();
+             ++is,++ic)
+        {
+                if( PQgetisnull(result,row_number, ic) ) continue;
+                char* value = PQgetvalue(result, row_number,ic);
+                dest[*is] = value;
+        }
+}    
+    
+template <typename T>
+void fetch_row(const PGresult* result, int row_number, T& dest) {
+        symbol_vector symbols;
+        detail::get_symbols(result, symbols);
+        fetch(result, row_number, symbols, dest);
+}
+void fetch_row(const PGresult* result, int row_number, result_map& dest)
+{
+        symbol_vector symbols;
+        detail::get_symbols(result, symbols);
+        fetch(result, row_number, symbols, dest);
+}
+
+
+template <typename T>
+void fetch_all(const PGresult* result, std::vector<T>& dest)
+{
+        int row_count = PQntuples(result);
+        dest.reserve(row_count);
+        
+        symbol_vector symbols;
+        detail::get_symbols(result, symbols);
+        std::back_insert_iterator<std::vector<T> > inserter(dest);
+        for(int row = 0; row < row_count; ++row,++inserter ) {
+                fetch_row(result, row, symbols, *inserter);
+                
+        }
+}
+
+
+} }
+
+#endif /*__tinfra_postgres_h__ */
