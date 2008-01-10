ffdb566b49579c053f53cb64b1e7ba88823dafa1
zbigg <zbigg@zbigg.org>
2008-01-10 13:31:42 +0000

    * test: added custom test reporter with OutDebugString and others

--- tinfra/test_main.cpp
+++ tinfra/test_main.cpp
@@ -8,6 +8,67 @@
 #include "tinfra/test.h"
 #include "tinfra/cmd.h"
 
+#include <unittest++/TestReporter.h>
+#include <unittest++/TestDetails.h>
+#include <cstdio>
+#include <cstddef>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+static void out(const char* message, ...)
+{
+    char buf[2048];
+    va_list ap;
+    va_start(ap, message);
+    std::vsprintf(buf,message, ap);
+    va_end(ap);
+    std::printf("%s",buf);
+#ifdef _WIN32
+    OutputDebugString(buf);
+#endif
+}
+
+class TinfraTestReporter: public UnitTest::TestReporter {
+public:    
+    void ReportFailure(UnitTest::TestDetails const& details, char const* failure)
+    {
+
+#ifdef __APPLE__
+        char const* const errorFormat = "%s:%d: error: FAILURE in %s: %s\n";
+#else
+        char const* const errorFormat = "%s(%d): error: FAILURE in %s: %s\n";
+#endif
+        out(errorFormat, details.filename, details.lineNumber, details.testName, failure);
+    }
+
+    void ReportTestStart(UnitTest::TestDetails const& test)
+    {
+        if( strcmp("DefaultSuite",test.suiteName) != 0 ) {
+            out("TEST %s::%s\n", test.suiteName, test.testName);
+        } else {
+            out("TEST %s\n", test.testName);
+        }
+    }
+
+    void ReportTestFinish(UnitTest::TestDetails const& /*test*/, float)
+    {
+    }
+
+    void ReportSummary(int totalTestCount, int failedTestCount,
+                       int failureCount, float secondsElapsed)
+    {
+        if (failureCount > 0)
+            out("FAILURE: %d out of %d tests failed (%d failures).\n", failedTestCount, totalTestCount, failureCount);
+        else
+            out("Success: %d tests passed.\n", totalTestCount);
+        out("Test time: %.2f seconds.\n", secondsElapsed);
+    }
+};
+
+
+
 #ifdef SRCDIR
 static std::string test_resources_dir = SRCDIR "/tinfra/test_resources";
 #else
@@ -16,7 +77,8 @@ static std::string test_resources_dir =  "tinfra/test_resources";
 
 int test_main(int argc, char** argv)
 {
-    return UnitTest::RunAllTests();
+    TinfraTestReporter reporter;
+    return UnitTest::RunAllTests(reporter, UnitTest::Test::GetTestList(), 0);
 }
 
 int main(int argc, char** argv)
