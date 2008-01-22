ad1b9fb86666b08dedb35f6ee48462fa7dd7428b
zbigg <zbigg@zbigg.org>
2008-01-22 23:01:50 +0000

    server: moved to new file tinfra/server.h

---
+++ tinfra/server.h
@@ -0,0 +1,34 @@
+#ifndef __tinfra_server_h__
+#define __tinfra_server_h__
+
+#include <memory>
+#include <string>
+#include "tinfra/io/stream.h"
+
+namespace tinfra {
+namespace net {
+    
+typedef tinfra::io::stream stream;
+
+class Server {
+    
+
+    std::auto_ptr<stream> server_socket_;
+    volatile bool stopped_;
+    std::string   bound_address_;
+    int           bound_port_;
+public:
+    Server();
+    Server(const char* address, int port);
+    virtual ~Server() {}
+    void bind(const char* address, int port);
+    void run();
+    void stop();
+    bool stopped() const { return stopped_; }
+protected:
+
+    virtual void onAccept(std::auto_ptr<stream> client) = 0;
+};
+
+} }
+#endif
