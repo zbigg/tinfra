5b20315d3643bf2a28d93f0293433171e4d8b841
zbigg <zbigg@zbigg.org>
2008-04-21 20:42:34 +0000

    runtime refactoring

--- tinfra/exception.cpp
+++ tinfra/exception.cpp
@@ -1,43 +1,9 @@
-#include <exception>
-#include <iostream>
-#include <iomanip>
-#include <string>
-#include <vector>
-
-#include "exception.h"
-
-#ifdef linux
-#include <execinfo.h>
-#include <signal.h>
-#define HAVE_BACKTRACE
-#endif
-
 namespace tinfra {
-
-void populate_stacktrace(stacktrace_t& dest,int ignore_stacks)
-{
-#ifdef HAVE_BACKTRACE
-    // Reference
-    // http://www-128.ibm.com/developerworks/library/l-cppexcep.html?ca=dnt-68
-    // http://www.delorie.com/gnu/docs/glibc/libc_665.html
-    //
-    void* addresses[256];
-    int size = ::backtrace(addresses, 256);
-    char** symbols = ::backtrace_symbols(addresses,size);
-    for( int i = ignore_stacks; i < size; i++ ) {
-	stackentry a;
-	a.address = addresses[i];
-	a.symbol = symbols[i];
-	dest.push_back(a);
-    }
-    ::free(symbols);
-#endif
-}
-
+    
 void print_stacktrace(std::ostream& out, int ignore_frames)
 {
     stacktrace_t stacktrace;
-    populate_stacktrace(stacktrace,ignore_frames);
+    get_stacktrace(stacktrace,ignore_frames);
 
     for( stacktrace_t::const_iterator i = stacktrace.begin(); i != stacktrace.end(); ++i ) {
 		out << "0x" << std::setfill('0') << std::setw(sizeof(i->address)) << std::hex << i->address 
@@ -59,5 +25,4 @@ generic_exception::generic_exception(std::string const& message): _message(messa
     */
 }
 
-
-} // end of namespace tinfra
+}
---
+++ tinfra/posix/runtime.cpp
@@ -0,0 +1,48 @@
+#include <exception>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+#include "exception.h"
+
+#ifdef linux
+#include <execinfo.h>
+#include <signal.h>
+#define HAVE_BACKTRACE
+#endif
+
+namespace tinfra {
+
+bool is_stacktrace_supported()
+{
+#ifdef HAVE_BACKTRACE
+    return true;
+#else
+    return false;
+#endif
+}
+bool get_stacktrace(stacktrace_t& t) 
+{
+#ifdef HAVE_BACKTRACE
+    // Reference
+    // http://www-128.ibm.com/developerworks/library/l-cppexcep.html?ca=dnt-68
+    // http://www.delorie.com/gnu/docs/glibc/libc_665.html
+    //
+    void* addresses[256];
+    int size = ::backtrace(addresses, 256);
+    char** symbols = ::backtrace_symbols(addresses,size);
+    for( int i = ignore_stacks; i < size; i++ ) {
+	stackentry a;
+	a.address = addresses[i];
+	a.symbol = symbols[i];
+	dest.push_back(a);
+    }
+    ::free(symbols);
+    return true;
+#else
+    return false;
+#endif
+}
+
+} // end of namespace tinfra
