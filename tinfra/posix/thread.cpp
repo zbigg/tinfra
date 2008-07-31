7e656c476d33c36294987e9926663f0f89feb5a5
zbigg <zbigg@zbigg.org>
2008-07-31 21:07:27 +0000

    * general win32/wine compatibility fixes

--- tinfra/posix/thread.cpp
+++ tinfra/posix/thread.cpp
@@ -116,7 +116,9 @@ void* Thread::join()
 
 void Thread::sleep(long milliseconds)
 {
-#if defined(HAVE_NANOSLEEP)
+#if defined(WIN32)
+    ::Sleep(milliseconds);
+#elif defined(HAVE_NANOSLEEP)
     // nanosleep accepts nanoseconds
     timespec req,rem;
     req.tv_sec  = milliseconds / 1000000000;
@@ -125,8 +127,6 @@ void Thread::sleep(long milliseconds)
 #elif defined(HAVE_USLEEP)
     // usleep accepts microseconds
     ::usleep(milliseconds/1000);
-#elif defined WIN32
-    ::Sleep(milliseconds);
 #else
     thread_error("sleep not implemented on this platform",0);
 #endif
