4c96222a462efe30b33eb91ca647dd174cc24c1e
zbigg <zbigg@zbigg.org>
2008-08-25 21:41:22 +0000

    * IOReactor::listen_channel - tweaked interface * (new) ProtocolHandler: interface draft * (new) ProtocolWrapperChannel: async protocol channel base implementation draft

--- reactor/reactor.cpp
+++ reactor/reactor.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <stdexcept>
 #include <list>
+#include <map>
 #include <poll.h>
 
 #include <iostream>
@@ -26,13 +27,13 @@ void remove(C1& victim, C2 const& to_remove)
 class IOChannel;
 
 class IOReactor {
+public:
     enum {
         READ = 1, WRITE = 2
     };
-public:
     virtual void add_channel(IOChannel* c) = 0;
     virtual void remove_channel(IOChannel* c) = 0;
-    virtual void listen_channel(IOChannel* c, int flags) = 0;
+    virtual void listen_channel(IOChannel* c, int mask, bool enable) = 0;
 
     virtual ~IOReactor() {}
 };
@@ -77,17 +78,22 @@ public:
     virtual void add_channel(IOChannel* c) 
     {
         channels.push_back(c);
+        channel_props[c] = 0;
     }
     
-    virtual void listen_channel(IOChannel* c,int flags) 
-    {
-        // TODO: add list of flags for each channel
+    virtual void listen_channel(IOChannel* c, int flags, bool enable) 
+    {    
+        if( enable ) {
+            channel_props[c] |=  flags;
+        } else {
+            channel_props[c] &= ~flags;
+        }
     }
 
     void cleanup() {
         for(ChannelsList::const_iterator i = to_remove.begin(); i != to_remove.end(); ++i ) {
             IOChannel* channel = *i;
-            
+            channel_props.erase(channel);
             channels.erase(std::find(channels.begin(), channels.end(), channel));
         }
         
@@ -155,7 +161,7 @@ private:
 
     ChannelsList channels;
     ChannelsList to_remove;
-    
+    std::map<IOChannel*,int> channel_props;
     void make_fds(PollIOReactor::ChannelsList const& channels, array<pollfd>& result)
     {    
         result.size(channels.size());
@@ -165,7 +171,13 @@ private:
         {
             IOChannel::ptr c = *i;
             result[k].fd = c->file();
-            result[k].events = POLLIN | POLLOUT; // TODO: use list of flags for each channel
+            int props = channel_props[c];
+            int events = 0;            
+            if( props && IOReactor::READ )
+                events |= POLLIN;
+            if( props && IOReactor::WRITE )
+                events |= POLLOUT;
+            result[k].events = events;
             result[k].revents = 0;
         }
     }
@@ -198,13 +210,15 @@ public:
     void data_available(IOReactor&) 
     {
         int new_socket;
+        std::string peer_address;
         {
-            tinfra::io::stream* s = tinfra::io::socket::accept_client_connection(main_socket);
+            
+            tinfra::io::stream* s = tinfra::io::socket::accept_client_connection(main_socket, &peer_address);
             new_socket = s->native();
             s->release();
             delete s;
         }
-        on_accept(new_socket,"");
+        on_accept(new_socket, peer_address);
     }
     
 protected:
@@ -225,6 +239,112 @@ protected:
     
 };
 
+class ProtocolHandler {
+public:
+    /// Called by ProtocolManager when IO has some data buffered
+    /// Protocol should consume as much data as he can
+    /// And then return number of bytes consumed    
+    /// @returns 0 if that protocol is unable to assemble any message - IO must gather more data
+    /// @returns > 0 length of consumed message
+    virtual int  accept_bytes(const char* , int) = 0;
+    
+    virtual void eof(int direction) = 0;
+
+    /// check if this protocol handler has finished reading
+    virtual bool is_finished() = 0;
+};
+
+typedef std::string buffer;
+
+class ProtocolWrapperChannel: public IOChannel {
+public:
+    ProtocolWrapperChannel(tinfra::io::stream* channel, ProtocolHandler* handler)
+        : channel(channel), 
+          handler(handler),
+          read_eof(false)
+    {}
+    
+protected:
+    tinfra::io::stream* channel;
+    
+    ProtocolHandler* handler;
+    
+    buffer received_bytes;
+    buffer to_send;
+
+    bool   read_eof;
+    bool   write_eof;
+
+    virtual int  file() { 
+        return channel->native(); 
+    }
+    
+    virtual void close() {
+        channel->close();
+    }
+    
+    void data_available(IOReactor& r)
+    {
+        // general idea
+        //  - read till end of buffer
+        //  - while possible consume using protocol handler
+        //    
+        while (! read_eof && ! handler->is_finished() ) {
+            while( ! received_bytes.empty() ) {
+                int accepted = handler->accept_bytes(received_bytes.data(), received_bytes.size());
+                if( accepted == 0 ) 
+                    break; // not nough data, try read some
+                received_bytes.erase(received_bytes.begin(), received_bytes.begin() + accepted);
+            }
+            
+            try {
+                char buffer[1024];
+                int readed = channel->read(buffer, sizeof(buffer));
+                if( readed == 0 ) {
+                    handler->eof(IOReactor::READ);
+                    read_eof = true;
+                    break;
+                }
+                received_bytes.append(buffer, readed);
+            } catch( tinfra::io::would_block& b) {
+                // nothing read so protocol can't continue
+                break;
+            }
+            // something read, retry with protocol
+        }
+        if( read_eof || handler->is_finished() ) {
+            r.listen_channel(this, IOReactor::READ, false);
+        } else {
+            r.listen_channel(this, IOReactor::READ, true);
+        }
+    }
+    
+    void write_possible(IOReactor& r)
+    {
+        try {
+            if( ! write_eof && to_send.size() > 0 ) {
+                int written = channel->write(to_send.data(), to_send.size());
+                if( written == 0 ) {
+                    handler->eof(IOReactor::WRITE);
+                    write_eof = true;
+                    break;
+                }
+                if( written > 0 ) {
+                    to_send.erase(to_send.begin(), to_send.begin() + written);
+                }
+            }
+        } catch( tinfra::io::would_block&) {
+            // ignore it!
+        }
+        
+        if( write_eof || to_send.size() == 0 ) {
+            r.listen_channel(this, IOReactor::WRITE, false);
+        } else {
+            r.listen_channel(this, IOReactor::WRITE, true);
+        }
+    }
+};
+
 int tinfra_main(int argc, char** argv)
 {
     PollIOReactor ctx;
@@ -233,6 +353,7 @@ int tinfra_main(int argc, char** argv)
     ctx.timeout = -1;
     
     ctx.add_channel( & echo_service ) ;
+    ctx.listen_channel( & echo_service, IOReactor::READ, true);
     
     ctx.loop();
     return 0;
