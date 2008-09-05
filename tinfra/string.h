451f18d2cba48e996273dae4d28399c66bcd102d
zbigg <zbigg@zbigg.org>
2008-09-05 06:58:42 +0000

    * tinfra::chop() - strip newline from end of string (added) * test_string: improve - test also inplace versions of strip/escape_c/chop

--- tinfra/string.h
+++ tinfra/string.h
@@ -12,6 +12,9 @@ std::string escape_c(const std::string& a);
 void        strip_inplace(std::string& s);
 std::string strip(const std::string& s);
 
+void        chop_inplace(std::string& s);
+std::string chop(std::string const& s);
+
 std::vector<std::string> split(std::string const& in, const char* delimiters);
 std::vector<std::string> split_lines(std::string const& in);
     
