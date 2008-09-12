8c1290fa710bcb2977693d066661ea5155d53074
zbigg <zbigg@zbigg.org>
2008-09-12 23:28:02 +0000

    regexp - splitted to separate h/impl

--- tstring/regex_pcre.cpp
+++ tstring/regex_pcre.cpp
@@ -1,250 +1,27 @@
 #include <pcre.h>
 
 #include <string>
-#include <vector>
 
 #include <iostream>
-#include <stdexcept>
+#include <assert.h>
 
-#include "tinfra/fmt.h"
 #include "tinfra/string.h"
 #include "tinfra/cmd.h"
 
-class regexp {
-    ::pcre*       re_;
-    ::pcre_extra* extra_;
-    size_t     patterns_count_;
-public:
-    typedef std::vector<std::string> match_result;
-    
-    regexp(const char* pattern);
-    ~regexp();
-    
-    bool matches(const char* str, size_t length) const {
-        return do_match(0, str, length, 0);
-    }
-    bool matches(const char* str, size_t length, match_result& result) const {
-        return do_match(&result, str, length, 0);
-    }
-    
-    bool matches(const char* str) const {
-        return do_match(0, str, std::strlen(str),0 );
-    }
-    
-    bool matches(const char* str, match_result& result) const  {
-        return do_match(&result, str, std::strlen(str), 0 );
-    }
-    
-    bool do_match(match_result* result, const char* str, size_t length, size_t* finish_offset) const;
-    
-private:
-    void compile(const char* pattern, int options);
-};
-
-class matcher {
-    regexp const& re_;
-    const char* str_;
-    size_t length_;
-    size_t position_;
-    bool have_result_;
-    bool have_match_;
-    regexp::match_result match_;
-public:
-    matcher(regexp const& re, const char* str, size_t length);
-    matcher(regexp const& re, const char* str);
-    
-    regexp::match_result const& next();
-    
-    bool has_next();
-
-private:
-    void try_match();
-};
-
-//
-// implementation
-//
-
-//
-// regexp
-//
-using tinfra::fmt;
-
-regexp::regexp(const char* pattern):
-    re_(0),
-    extra_(0),
-    patterns_count_(0)
-{
-    compile(pattern, 0);
-}
-
-regexp::~regexp()
-{
-    if( extra_ )
-        pcre_free(extra_);
-    if( re_ )
-        pcre_free(re_);
-}
-
-void regexp::compile(const char* pattern, int options)
-{
-    const char* err_ptr = 0;
-    int         err_offset;
-    
-    options = 0; //PCRE_NEWLINE_ANY;
-    
-    re_ = pcre_compile(pattern, options, &err_ptr, &err_offset,0);
-    if( re_ == 0 ) {
-        if( err_ptr == 0 ) 
-            err_ptr = "unknown error";
-        throw std::logic_error(fmt("bad regular expression '%s': %s") % pattern % err_ptr);
-    }
-    extra_ = pcre_study(re_, 0,  &err_ptr);
-    if( extra_ == 0 && err_ptr != 0 ) {
-        throw std::logic_error(fmt("pcre_study failed: %s") % err_ptr);
-    }
-    
-    pcre_fullinfo(re_, extra_, PCRE_INFO_CAPTURECOUNT, &patterns_count_);
-}
-
-
-bool regexp::do_match(match_result* result, const char* str, size_t length, size_t* finish_offset) const
-{
-    const int offsets_size = (patterns_count_+1)*3; // see manual, pcre_exec def
-    int offsets[offsets_size]; 
-    int options = 0;
-    int rc = pcre_exec(re_, extra_, str, length, 0, options, 
-                       offsets, offsets_size);
-    if( rc == -1 )
-        return false;
-    
-    if( rc < 0 ) {
-        const char* err_ptr = "unknown error";
-        throw std::logic_error(fmt("PCRE match failed: %s") % err_ptr);
-    }
-    if( result != 0 ) {
-        match_result& r = *result;
-        r.resize(patterns_count_+1);
-        for( size_t i = 0; i <= patterns_count_; ++i ) {
-            const char* p = str + offsets[i*2];
-            const char* e = str + offsets[i*2 +1];
-            const size_t len = e-p;
-            r[i].assign(p, len);
-        }
-    }
-    if( finish_offset != 0 ) {
-        *finish_offset = offsets[1];
-    }
-    return true;
-}
-
-//
-// matcher
-//
-
-matcher::matcher(regexp const& re, const char* str, size_t length):
-    re_(re),
-    str_(str),
-    length_(length),
-    position_(0),
-    have_result_(false),
-    have_match_(false)
-{}
-
-matcher::matcher(regexp const& re, const char* str):
-    re_(re),
-    str_(str),
-    length_(std::strlen(str)),
-    position_(0),
-    have_result_(false),
-    have_match_(false)
-{}
-
-regexp::match_result const& matcher::next() {
-    if( ! have_result_ ) 
-        try_match();
-    if( have_match_ ) {
-        have_result_ = false;
-        return match_;
-    } else {
-        throw std::runtime_error("no more matches");
-    }
-}
-
-bool matcher::has_next()
-{
-    if( !have_result_ ) 
-        try_match();
-    return have_match_;
-}
-
-
-void matcher::try_match()
-{
-    have_result_ = true;
-    if( position_ == length_ ) {
-        have_match_ = false;
-    } else {
-        size_t match_end;
-        have_match_ = re_.do_match(&match_, 
-                                   str_ + position_, length_ - position_,
-                                   &match_end);
-        position_ = position_ + match_end;
-    }
-}
-
-//
-// scanner
-//
-
-class scanner {
-    regexp::match_result match_;
-    size_t  current_param_;
-    bool    have_match_;
-public:
-    scanner(regexp const& re, const char* str):  
-        current_param_(0),
-        have_match_(re.matches(str, match_))
-    {
-    }
-    scanner(regexp const& re, const char* str, size_t length):  
-        current_param_(0),
-        have_match_(re.matches(str, length, match_))
-    {
-    }
-    
-    bool matches() const { return have_match_; }
-    
-    operator bool() const { return matches(); }
-    
-    template <typename T>
-    scanner& push(T& value) {
-        if( !have_match_ ) 
-            return *this;
-        current_param_++;
-        if( current_param_ == match_.size() ) {
-            throw std::logic_error("scanner: too many arguments");
-        }
-        tinfra::from_string<T>(match_[current_param_], value);        
-        return *this;
-    }
-    
-    template <typename T>
-    scanner& operator <<(T& value) { return push(value); }
-    
-    template <typename T>
-    scanner& operator %(T& value) { return push(value); }
-};
+#include "regex.h"
 
 //
 // sample program, proof of concept
 //
 
+using tinfra::regexp;
+using tinfra::scanner;
+using tinfra::matcher;
 void test_scanner()
 {
     std::string name;
     int h,m,s;
-    bool matches = scanner("^(\\w+) (\\d+):(\\d+):(\\d+)$", "Week 1:22:333") % name % h % m % s % s;
+    bool matches = scanner("^(\\w+) (\\d+):(\\d+):(\\d+)$", "Week 1:22:333") % name % h % m % s;
     
     assert(matches);
     assert(name=="Week");
---
+++ tstring/regexp.h
@@ -0,0 +1,117 @@
+#ifndef __tinfra_regex_h__
+#define __tinfra_regex_h__
+
+#define HAVE_PCRE 1
+#ifdef HAVE_PCRE
+#define TINFRA_REGEX_PCRE 1
+#include <pcre.h>
+#else
+#endif
+
+#include <string>
+#include <vector>
+
+namespace tinfra {
+    
+class regexp {
+    
+#ifdef TINFRA_REGEX_PCRE
+    ::pcre*       re_;
+    ::pcre_extra* extra_;
+#endif
+    
+    size_t     patterns_count_;
+public:
+    typedef std::vector<std::string> match_result;
+    
+    regexp(const char* pattern);
+    ~regexp();
+    
+    bool matches(const char* str, size_t length) const {
+        return do_match(0, str, length, 0);
+    }
+    bool matches(const char* str, size_t length, match_result& result) const {
+        return do_match(&result, str, length, 0);
+    }
+    
+    bool matches(const char* str) const {
+        return do_match(0, str, std::strlen(str),0 );
+    }
+    
+    bool matches(const char* str, match_result& result) const  {
+        return do_match(&result, str, std::strlen(str), 0 );
+    }
+    
+    bool do_match(match_result* result, const char* str, size_t length, size_t* finish_offset) const;
+    
+private:
+    void compile(const char* pattern, int options);
+};
+
+class matcher {
+    regexp const& re_;
+    const char* str_;
+    size_t length_;
+    size_t position_;
+    bool have_result_;
+    bool have_match_;
+    regexp::match_result match_;
+public:
+    matcher(regexp const& re, const char* str, size_t length);
+    matcher(regexp const& re, const char* str);
+    
+    regexp::match_result const& next();
+    
+    bool has_next();
+
+private:
+    void try_match();
+};
+
+//
+// scanner
+//
+
+class scanner {
+    regexp::match_result match_;
+    size_t  current_param_;
+    bool    have_match_;
+public:
+    scanner(regexp const& re, const char* str):  
+        current_param_(0),
+        have_match_(re.matches(str, match_))
+    {
+    }
+    scanner(regexp const& re, const char* str, size_t length):  
+        current_param_(0),
+        have_match_(re.matches(str, length, match_))
+    {
+    }
+    
+    bool matches() const { return have_match_; }
+    
+    operator bool() const { return matches(); }
+    
+    template <typename T>
+    scanner& parse(T& value);
+    
+    template <typename T>
+    scanner& operator >>(T& value) { return parse(value); }
+    
+    template <typename T>
+    scanner& operator %(T& value) { return parse(value); }
+private:
+    bool check_and_forward();
+};
+
+template <typename T>
+scanner& scanner::parse(T& value) {
+    if( check_and_forward() ) {
+        tinfra::from_string<T>(match_[current_param_], value);
+    }
+    return *this;
+}
+
+}  // end namespace tinfra
+
+#endif // __tinfra_regex_h__
