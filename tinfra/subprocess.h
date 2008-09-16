9658d8c64d63cec660face12ef33f6fc21dc4472
zbigg <zbigg@zbigg.org>
2008-09-16 06:47:32 +0000

    copyright holder info added, GPLv2 applied to all

--- tinfra/subprocess.h
+++ tinfra/subprocess.h
@@ -1,3 +1,10 @@
+//
+// Copyright (C) Zbigniew Zagorski <z.zagorski@gmail.com>,
+// licensed to the public under the terms of the GNU GPL (>= 2)
+// see the file COPYING for details
+// I.e., do what you like, but keep copyright and there's NO WARRANTY.
+//
+
 #ifndef __tinfra_subprocess_h__
 #define __tinfra_subprocess_h__
 
