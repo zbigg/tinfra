3089951824f2dcd943be0716c8bb931012f8f547
zbigg <zbigg@zbigg.org>
2008-10-22 14:36:41 +0000

    sftp_vfs implementation start

--- sftp/sftp.cpp
+++ sftp/sftp.cpp
@@ -106,7 +106,7 @@ std::string hexify(const char* buf, size_t length)
 
 
 template <typename T>
-void send(tinfra::subprocess* p, T const& packet)
+void send(tinfra::io::stream* to_server, T const& packet)
 {
     std::string packet_buffer;
         
@@ -125,7 +125,7 @@ void send(tinfra::subprocess* p, T const& packet)
                           sizeof(uint32));
     
     DOUT( "writing packet: " << struct_to_string(packet) );    
-    p->get_stdin()->write( packet_buffer.data(), packet_buffer.size() );
+    to_server->write( packet_buffer.data(), packet_buffer.size() );
     DOUT( "written bytes: " << hexify(packet_buffer.data(), packet_buffer.size()) );
 }
 
@@ -160,15 +160,15 @@ void expect_header(tinfra::io::stream* s, packet_header& result, byte expected_t
 }
 
 template <typename T>
-void expect(tinfra::subprocess* p, T& packet)
+void expect(tinfra::io::stream* from_server, T& packet)
 {
     packet_header header;
-    expect_header(p->get_stdout(), header, T::type);
+    expect_header(from_server, header, T::type);
     
     const int packet_length = header.length - sizeof(sftp::byte);
     char packet_buf[packet_length];
     
-    read_for_sure(p->get_stdout(), packet_buf, packet_length);
+    read_for_sure(from_server, packet_buf, packet_length);
     
     DOUT( "readed  packet: " << hexify(packet_buf, packet_length) );
     
@@ -177,17 +177,152 @@ void expect(tinfra::subprocess* p, T& packet)
     DOUT( "decoded packet: " << struct_to_string(packet));
 }
 
+class sftp_vfs {
+public:
+    virtual ~vfs() {}
+    
+    virtual tinfra::fs::file_name_list roots()
+    {
+    }
+    
+    virtual void list_files(const char* path, tinfra::fs::file_list_visitor& visitor)
+    {
+        std::string handle
+        {            
+            open_dir_packet request;
+            request.request_id = get_next_request_id();
+            request.path = path;
+            
+            send(to_server, request);
+            
+            handle_packet response;
+            expect(from_server, response);
+            
+            handle = response.handle;
+        }
+        {
+            read_dir_packet request;
+            request.request_id = get_next_request_id();
+            request.handle = handle;
+            send(to_server, request);
+            
+            name_packet response;
+            expect(from_server, response);
+            
+            for( prefixed_list<name_element>::const_iterator i = response.elements.begin();
+                 i != response.elements.end();
+                 ++i )
+            {
+                visitor.accept(i->filename.c_str());
+            }
+        }
+        { // TODO: this handle should be closed even in case of exception 
+          // unconditionally!
+            close_packet request;
+            request.request_id = get_next_request_id();
+            request.handle = handle;
+            
+            send(to_server, request);
+            
+            status_packet response;
+            expect(from_server, response);
+        }
+    }
+    
+    virtual tinfra::fs::file_info stat(const char* path)
+    {
+        stat_packet request;
+        request.request_id = get_next_request_id();
+        request.path = path;
+        
+        send(to_server, request);
+        
+        attrs_packet response;
+        expect(from_server), response);
+        
+        tinfra::fs::file_info result;
+        result.size   = response.size;
+        result.is_dir = S_ISDIR(response.permissions);
+        result.modification_time = response.mtime;
+        result.access_time = response.atime;
+        
+        return result;
+    }
+    
+    virtual tinfra::io::stream* open(const char* path, tinfra::io::openmode mode)
+    {
+    }
 
-int sftp_main(int argc, char** argv)
-{
-    tinfra::set_interrupt_policy(tinfra::DEFERRED_SIGNAL);
-    std::auto_ptr<tinfra::subprocess> sp = tinfra::subprocess::create();
+    virtual void rm(const char* name)
+    {
+    }
+
+    virtual void rmdir(const char* name)
+    {
+    }
     
-    sp->set_stdout_mode(tinfra::subprocess::REDIRECT);
-    sp->set_stdin_mode(tinfra::subprocess::REDIRECT);
+    virtual void mkdir(const char* name)
+    {
+    }
+        
+    virtual void copy(const char* src, const char* dest)
+    {
+    }
+
+    virtual void mv(const char* src, const char* dst)
+    {
+    }
     
-    sp->start("ssh -s localhost sftp");
+    virtual void recursive_copy(const char* src, const char* dest)
+    {
+    }
     
+    virtual void recursive_rm(const char* src)
+    {
+    }
+        
+    virtual bool is_file(const char* name)
+    {
+    }
+    virtual bool is_dir(const char* name)
+    {
+    }
+    virtual bool exists(const char* name)
+    {
+    }
+private:
+    
+    sftp_vfs(std::string const& target, std::string const& command):
+        base_command(command),
+        target(target),
+        ssh(tinfra::subprocess::create()),
+        to_server(0), from_server(0),
+        next_request_id(0)
+    {
+        start();
+    }
+    
+    void close()
+    {
+        to_server->close();
+        ssh->wait();
+    }
+    
+    void start()
+    {
+        string command = tinfra::fmt("%s %s sftp") % base_command % target;
+        ssh->set_stdout_mode(tinfra::subprocess::REDIRECT);
+        ssh->set_stdin_mode(tinfra::subprocess::REDIRECT);
+    
+        ssh->start("ssh -s localhost sftp");
+        
+        from_server = ssh->get_stdout();
+        to_server   = ssh->get_stdin();
+        
+        initialize_protocol();
+    }
+    
+    void initialize_protocol()
     {
         init_packet init;
         init.version = 2;
@@ -197,6 +332,28 @@ int sftp_main(int argc, char** argv)
         version_packet version;
         expect(sp.get(), version);
     }
+    
+    int get_next_request_id() {
+        return ++next_request_id;
+    }
+    
+    std::string base_command;
+    std::string target;
+    
+    std::auto_ptr<tinfra::subprocess> ssh;
+    
+    tinfra::io::stream* from_server;
+    tinfra::io::stream* to_server;
+    
+    int next_request_id;
+};
+
+
+int sftp_main(int argc, char** argv)
+{
+    tinfra::set_interrupt_policy(tinfra::DEFERRED_SIGNAL);
+    
+    sftp_vfs fs("localhost", "ssh -s");
     
     {
         stat_packet request;
