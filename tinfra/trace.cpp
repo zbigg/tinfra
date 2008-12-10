6c4c74d3eded46ecd22ed25e307703fa05262e88
zbigg <zbigg@zbigg.org>
2008-12-10 15:24:43 +0000

    added trace experimental code, ouch

---
+++ tinfra/trace.cpp
@@ -0,0 +1,16 @@
+#include "tinfra/trace.h"
+
+#include <sstream>
+#include <iostream>
+
+#include "tinfra/fmt.h"
+#include "tinfra/string.h"
+
+namespace tinfra {
+    
+    
+void trace(const char* file_name, int line, const char* message)
+{
+}
+
+};
