2daa37db7f48fab778649bf1e2a6db9bfde8cecf
zbigg <zbigg@zbigg.org>
2008-12-17 09:38:14 +0000

    moved from tinfra.sandbox

---
+++ tinfra/interruptible.h
@@ -0,0 +1,78 @@
+//
+// Copyright (C) 2008 Zbigniew Zagorski <z.zagorski@gmail.com>,
+// licensed to the public under the terms of the GNU GPL (>= 2)
+// see the file COPYING for details
+// I.e., do what you like, but keep copyright and there's NO WARRANTY.
+//
+
+#ifndef __tinfra_interruptible_h__
+#define __tinfra_interruptible_h__
+
+#include <stdexcept>
+
+namespace tinfra {
+    
+template<typename R, typename T>
+class interruptible {
+public:
+    typedef R (interruptible::*step_method)(T const& e);
+    
+    interruptible(step_method m = 0):
+        next_method_(m),
+        again_(false),
+        finished_(false)
+    {
+    }
+    
+    bool process(T const& input)
+    {
+        if( finished_ ) {
+            throw std::logic_error("already finished");
+        }
+        step_method current_method = next_method_;
+        next_method_ = 0;
+        again_ = false;
+        
+        R output = call(current_method, input);
+        
+        if( again_ ) {
+            next_method_ = current_method;
+        } else if( !next_method_ ) {
+            finished_  = true;
+        }
+        return output;
+    }
+    
+    bool is_finished() const {
+        return finished_;
+    }
+protected:
+    
+    R call(step_method m, T const& a)
+    {
+        return (this->*m)(a);
+    }
+    void next(step_method s) {
+        next_method_ = s;
+    }
+    
+    template <typename F>
+    step_method make_step_method(R (F::*m)(T const&)) {
+        return reinterpret_cast<step_method>(m);
+    }
+    void again() {
+        again_ = true;
+    }
+    
+    void finish() {
+        // nothing needed
+    }
+private:
+    step_method next_method_;
+    bool        again_;
+    bool        finished_;
+};
+
+} // end namespace tinfra
+
+#endif // __tinfra_interruptible_h__
---
+++ tinfra/lazy_protocol.cpp
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2008 Zbigniew Zagorski <z.zagorski@gmail.com>,
+// licensed to the public under the terms of the GNU GPL (>= 2)
+// see the file COPYING for details
+// I.e., do what you like, but keep copyright and there's NO WARRANTY.
+//
+
+#include "tinfra/lazy_protocol.h"
+
+namespace tinfra {
+
+/*
+private:
+step_method waiter_method_;
+size_t      waiter_count_;
+string      waiter_delim_;
+*/
+    
+void lazy_protocol::wait_for_bytes(size_t count, step_method method)
+{
+    waiter_count_ = count;
+    
+    waiter_method_ = method;        
+    next(make_step_method(&lazy_protocol::maybe_have_enough_bytes));
+}
+
+void lazy_protocol::wait_for_delimiter(tstring const& delim, step_method method)
+{
+    waiter_delim_.assign(delim.data(), delim.size());
+    waiter_method_ = method;
+    next(make_step_method(&lazy_protocol::maybe_have_delim));
+}
+
+
+int lazy_protocol::maybe_have_delim(tstring const& input)
+{
+    size_t pos =  input.find_first_of(waiter_delim_.data(), waiter_delim_.size());
+    if( pos == tstring::npos ) {
+        again();
+        return 0;
+    }
+    return call(waiter_method_, tstring(input.data(), pos));
+}
+
+int lazy_protocol::maybe_have_enough_bytes(tstring const& input)
+{
+    if( input.size() < waiter_count_ ) {
+        again();
+        return 0;
+    }
+    
+    return call(waiter_method_, tstring(input.data(), waiter_count_));
+}
+
+} // end namespace tinfra
