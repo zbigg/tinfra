96c07cb61f5dc2bac43eca4472062830a4be3c3c
zbigg <zbigg@zbigg.org>
2008-12-18 14:21:57 +0000

    db header start added

---
+++ tinfra/db.h
@@ -0,0 +1,24 @@
+//
+// Copyright (C) 2008 Zbigniew Zagorski <z.zagorski@gmail.com>,
+// licensed to the public under the terms of the GNU GPL (>= 2)
+// see the file COPYING for details
+// I.e., do what you like, but keep copyright and there's NO WARRANTY.
+//
+
+#ifndef __tinfra_db_h__
+#define __tinfra_db_h__
+
+#include <istream>
+
+namespace tinfra {
+
+// basic ways of accessing db:
+    
+    // get connection ->conn
+    // conn execute query -> result
+    // nullable values
+    
+} // end namespace tinfra
+
+#endif // __tinfra_db_h__
+
