17a58c1eb4baec7323b52e68ba3327932868be89
zbigg <zbigg@zbigg.org>
2009-01-07 22:22:51 +0000

    tinfra-support introduction

---
+++ spawn-test-common.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+dist="$1"
+if [ ! -d "$dist" ] ; then
+    echo "$0: output dir '$dist' doesn't exist, build the project" 1>&2  
+    exit 2
+fi
+test_log_dir=${dist}/test_result
+mkdir -p ${test_log_dir}
+
+interactive=no
+
+failed=0
+
+success() {
+    test_name="$1"
+    test_log="${test_log_dir}/${test_name}.log"
+    echo "-----------------" >> $test_log
+    shift
+    (
+        echo -e "${test_name}\t\t (success)"
+        if [ -n "$*" ] ; then
+            echo "${test_name}: $*"
+        fi
+    ) | tee -a ${test_log}
+}
+fail()
+{
+    test_name="$1"
+    test_log="${test_log_dir}/${test_name}.log"
+    echo "-----------------" >> $test_log
+    shift
+    (
+        echo -e "${test_name}\t\t (FAIL)"
+        if [ -n "$*" ] ; then
+            echo "${test_name}: $*"
+        fi
+    ) | tee -a ${test_log}
+    failed=1
+}
+
+generic_test()
+{
+    test_name="$1"
+    test_log="${test_log_dir}/${test_name}.log"
+    if $dist/${test_name} &> ${test_log} ; then
+        success ${test_name}
+    else
+        fail ${test_name}
+    fi
+}
