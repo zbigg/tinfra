36c1444041e0e75bad1ef1a3fb6459417e16d002
Zbigniew Zagorski <z.zagorski@gmail.com>
2009-04-02 17:03:01 +0000

    mo: field_printer moved from sandbox

--- tests/test_mo.cpp
+++ tests/test_mo.cpp
@@ -11,12 +11,9 @@
 
 #include "tinfra/mo.h"
 
-
-SUITE(tinfra)
-{
+namespace tinfra_mo_test {
     TINFRA_SYMBOL_IMPL(x);
     TINFRA_SYMBOL_IMPL(y);
-    
     struct point {
         int x;
         int y;
@@ -25,6 +22,18 @@ SUITE(tinfra)
             TINFRA_MO_FIELD(y);
         }
     };
+}
+
+namespace tinfra {
+    template<> 
+    struct mo_traits<tinfra_mo_test::point>: public tinfra::struct_mo_traits<tinfra_mo_test::point> {};
+}
+
+SUITE(tinfra)
+{
+    
+    
+    using tinfra_mo_test::point;
     using tinfra::symbol;
     
     struct dummy_functor {
@@ -51,4 +60,36 @@ SUITE(tinfra)
         CHECK_EQUAL(1, a.x);
         CHECK_EQUAL(1, a.y);
     }
+    
+    TEST(mo_field_printer_struct) {
+        std::ostringstream buf;
+        tinfra::field_printer printer(buf);
+        point X = {1,2};
+        tinfra::process(tinfra::symbol("X"), X, printer);
+        CHECK_EQUAL("X={ x=1, y=2 }", buf.str());
+    }
+    
+    TEST(mo_field_printer_container) {
+        std::ostringstream buf;
+        tinfra::field_printer printer(buf);
+        
+        std::vector<int> v;
+        v.push_back(1);
+        v.push_back(2);
+        v.push_back(3);
+        v.push_back(5);
+        
+        tinfra::process(tinfra::symbol("v"), v, printer);
+        CHECK_EQUAL("v=[ 1, 2, 3, 5 ]", buf.str());
+    }
+    
+    TEST(mo_field_printer_mo_container) {
+        std::ostringstream buf;
+        tinfra::field_printer printer(buf);
+        point X = {1,2};
+        std::vector<point> v(2, X);
+        tinfra::process(tinfra::symbol("v"), v, printer);
+        CHECK_EQUAL("v=[ { x=1, y=2 }, { x=1, y=2 } ]", buf.str());
+    }
+    
 }
--- tinfra/mo.h
+++ tinfra/mo.h
@@ -25,7 +25,6 @@ struct mo_traits {
     }
 };
 
-
 template <typename T>
 struct struct_mo_traits {
     template <typename Functor>
@@ -42,6 +41,29 @@ struct struct_mo_traits {
         f.end_mo(s, v);
     }
 };
+
+template <typename T>
+struct container_mo_traits{
+    template <typename Functor>
+    static void process(const symbol& s, const T& v, Functor& f);
+    
+    /*
+    template <typename Functor>
+    static void mutate(const symbol& s, T& v, Functor& f) { 
+        f.begin_container(s, v);
+        typedef typename T::value_type value_type;
+        typedef typename T::const_iterator  iterator;
+        while( f.hasNext() ) {
+            v.push_back(f.next());
+        }
+        f.end_container(s, v);
+    }
+    */
+};
+
+template <typename T>
+struct mo_traits < std::vector<T> >: 
+    public container_mo_traits< std::vector<T> > { };
 
 namespace mo {
 
@@ -103,12 +125,134 @@ void mutate(symbol const& sym,  T& value, F& functor)
     functor_disp(sym, value);
 }
 
+template <typename T>
+template <typename Functor>
+void container_mo_traits<T>::process(const symbol& s, const T& v, Functor& f) { 
+    f.begin_container(s, v);
+    typedef typename T::const_iterator  iterator;
+    for( iterator i = v.begin(); i != v.end(); ++i ) {
+        tinfra::process(symbol(0),*i, f);
+    }
+    f.end_container(s, v);
+}
 #define TINFRA_MO_MANIFEST(a)  template <typename F> void apply(F& f) const
 
 #define TINFRA_MO_FIELD(a)    f(S::a, a)
     
 #define TINFRA_SYMBOL_DECL(a) namespace S { extern tinfra::symbol a; }
 #define TINFRA_SYMBOL_IMPL(a) namespace S { tinfra::symbol a(#a); }
+
+class field_printer {
+    std::ostream& out;
+    bool need_separator;
+    int  indent_size;
+    int  indent_level;
+    bool multiline;
+    bool showing_name;
+    std::vector<bool> sn_history;
+public:
+    field_printer(std::ostream& o): 
+        out(o), 
+        need_separator(false),
+        indent_size(2),
+        indent_level(0), 
+        multiline(false),
+        showing_name(true)
+    {}
+    
+    template <class T>
+    void operator () (const tinfra::symbol& sym, T const& t) 
+    {
+        separate();
+        apply_indent();
+        name(sym);
+        out << t;
+        need_separator = true;
+    }
+    
+    template <typename T>
+    void begin_mo(tinfra::symbol const& sym, T const& v)
+    {
+        separate();
+        enter(sym, '{');
+        push_showing_name(true);
+    }
+    
+    template <typename T>
+    void end_mo(tinfra::symbol const& sym, T const& v)
+    {
+        pop_showing_name();
+        need_separator = false;
+        separate();
+        exit(sym,'}');
+    }
+    
+    template <typename T>
+    void begin_container(tinfra::symbol const& sym, T const& v)
+    {
+        separate();
+        enter(sym, '[');
+        push_showing_name(false);        
+    }
+    
+    template <typename T>
+    void end_container(tinfra::symbol const& sym, T const& v)
+    {
+        pop_showing_name();
+        need_separator = false;
+        separate();
+        exit(sym,']');
+    }
+private:
+    void push_showing_name(bool new_value) {
+        sn_history.push_back(showing_name);
+        showing_name = new_value;
+    }
+    void pop_showing_name() {
+        showing_name = sn_history.at(sn_history.size()-1);
+        sn_history.erase(sn_history.end()-1);
+    }
+    void name(symbol const& sym) {
+        if( showing_name ) {
+            out << sym << '=';
+        }
+    }
+    void separate() {
+        if( need_separator ) {
+            out << ", ";
+        }
+        if( multiline ) {
+            out << "\n";
+        }
+        need_separator = false;
+    }
+    void apply_indent() {
+        if( indenting() ) {
+            for(int i = 0; i < indent_level*indent_size; ++i )
+                out << " ";
+        }
+    }
+    void enter(tinfra::symbol const& sym, char sep) {
+        apply_indent();
+        name(sym);
+        out << sep;
+        indent_level+=1;
+        if( !multiline )
+            out << " ";
+        need_separator = false;
+    }
+    
+    void exit(tinfra::symbol const&, char sep) {
+        indent_level -= 1;
+        apply_indent();
+        if( !multiline )
+            out << ' ';
+        out << sep;
+        need_separator = true;
+    }
+    
+    bool indenting() const { return multiline && indent_level != 0 && indent_size > 0; }
+}; 
 
 } // end namespace tinfra
 
