1b2bc27fecff93c045c291644f0d60b3afa1f599
zbigg <zbigg@zbigg.org>
2009-10-19 19:38:54 +0000

    stream(input_stream,output_stream): implemented simple creators for file reader/writer

--- tinfra/stream.h
+++ tinfra/stream.h
@@ -6,18 +6,24 @@
 #ifndef tinfra_stream_h_included
 #define tinfra_stream_h_included
 
+#include "tstring.h" // for tstring
+#include <memory>  // for auto_ptr
+
 namespace tinfra {
 
 class input_stream {
 public:
-    virtual ~input_stream() { }
+    virtual ~input_stream();
+
     virtual void close() = 0;
+
     virtual int read(char* dest, int size) = 0;
 };
 
 class output_stream {
 public:
-    virtual ~output_stream() { }
+    virtual ~output_stream();
+
     virtual void close() = 0;
     
     virtual int write(const char* data, int size) = 0;
@@ -42,6 +48,16 @@ class position_controller {
     
     virtual int seek(int pos, seek_origin origin = start) = 0;
 };
+#endif
+
+std::auto_ptr<input_stream>  create_file_input_stream(tstring const& name);
+
+enum file_output_mode {
+    TRUNCATE = 1,
+    APPEND   = 2
+};
+
+std::auto_ptr<output_stream> create_file_output_stream(tstring const& name, int mode);
 
 } // end namespace tinfra
 
@@ -57,5 +73,4 @@ class position_controller {
 #endif // disabling of platform specific defs
 
 #endif // tinfra_stream_h_included
-
 
