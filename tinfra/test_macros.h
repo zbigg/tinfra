23fbde63eec867a76937fce4133ba675b30fb516
zbigg <zbigg@zbigg.org>
2010-12-14 07:47:30 +0000

    * build: added missing test_macros.h file * CHECK_MAP_EQUAL added (not tested)

--- tinfra/test_macros.h
+++ tinfra/test_macros.h
@@ -6,10 +6,90 @@
 #ifndef tinfra_test_macros_h_included
 #define tinfra_test_macros_h_included
 
+#include "fmt.h"
+
 // we're currently basing on UnitTest++, so include them!
 #include <unittest++/UnitTest++.h>
 #include <unittest++/TestMacros.h>
 #include <unittest++/CheckMacros.h>
 
-#endif // tinfra_test_macros_h_included
+namespace tinfra { 
+namespace test {
 
+/// Report test failure.
+///
+/// Reports a test failure described by message and source code location.
+///
+/// Currently UnitTest++ library current running test results is
+/// updated with this failure.
+void report_test_failure(const char* filename, int line, const char* message);
+
+/// Check associative container equailty.
+///
+/// Please don't use directly, use CHECK_EQUAL_MAPS macro.
+///
+/// Check that two associative container are equal by means of content.
+///
+/// Actual map is checked item by item and each inconsistency:
+///  - extra item
+///  - value item under key not maching expected value
+///  - missing items
+/// is reported as separate failure message.
+///
+/// This function must be used from within TEST macro execution as it uses
+/// static test infrastructure to report failures.
+///
+template <typename MapType>
+void check_map_equal(MapType const& A, 
+                     MapType const& B, std::string const& b_name, const char* filename, int line)
+{
+    typedef typename MapType::value_type value_type;
+    typedef typename MapType::key_type   key_type;
+    
+    typedef typename MapType::const_iterator iter;
+    
+    for( iter ia = A.begin(); ia != A.end(); ++ia ) {
+        iter ib = B.find(ia->first);
+        if( ib == B.end() ) {
+            std::string msg = tinfra::fmt("expected item (%s->%s) not found in %s") % ia->first % ia->second % b_name;
+            report_test_failure(filename, line, msg.c_str()); 
+        } else if ( !( ia->second == ib->second) ) {
+            std::string msg = tinfra::fmt("expected (%s->%s) but found (%s->%s) found in %s") 
+                % ia->first %ia->second 
+                % ib->first %ib->second 
+                % b_name;
+            report_test_failure(filename, line, msg.c_str()); 
+        }
+    }
+    
+    for( iter ib = B.begin(); ib != B.end(); ++ib ) {
+        iter ia = A.find(ib->first);
+        if( ia == A.end() ) {
+            std::string msg = tinfra::fmt("extra (%s->%s) found in %s") % ib->first %ib->second % b_name;
+            report_test_failure(filename, line, msg.c_str()); 
+        } 
+    }
+}
+
+/// Check associative container equailty.
+///
+/// Check that two associative container are equal by means of content.
+///
+/// Actual map is checked item by item and each inconsistency:
+///  - extra item
+///  - value item under key not maching expected value
+///  - missing items
+/// is reported as separate failure message.
+///
+/// This macro must be used from within TEST macro execution as it uses
+/// static test infrastructure to report failures.
+///
+/// @param expected    expected map contents (pattern)
+/// @param actual      actual map contents to be checked against pattern
+
+#define CHECK_EQUAL_MAPS(expected,actual) \
+    tinfra::test::check_map_equal(expected, actual, #actual, __FILE__, __LINE__)
+
+}}  // end namespace tinfra::test
+
+#endif // tinfra_test_macros_h_included
