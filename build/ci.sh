2dc7e7cbae2a6c34543be7e1ae98ea39e5421a4c
z.zagorski <z.zagorski@gmail.com>
2014-05-08 20:45:44 +0000

    build/ci script added for continuous integration

---
+++ build/ci.sh
@@ -0,0 +1,54 @@
+#!/usr//bin/env bash
+
+#
+# build/ci.sh
+#
+#   script that does various CI (Continous Integration) tasks for tinfra
+###
+# by default, it 
+#  - boostraps environment
+#  - configures
+#  - builds
+#  - makes fake installation
+#  - tests
+#  - makes distcheck
+#
+###
+
+header()
+{
+    echo "===================="
+    echo "| $@"
+    echo "--------------------"
+}
+
+set -e
+
+header "makefoo"
+if [ ! -d makefoo-master ] ; then
+    git clone https://github.com/zbigg/makefoo.git makefoo-master
+else
+    ( cd makefoo-master; git pull )
+fi
+
+header "aclocal"
+aclocal -I makefoo-master
+
+header "configure"
+./configure
+
+source makefoo_configured_defs.mk
+
+header "make"
+${MAKEFOO_MAKE}
+
+header "make install"
+${MAKEFOO_MAKE} install DESTDIR=`pwd`/.tmp-installation
+rm -rf .tmp-installation
+
+header "make test"
+${MAKEFOO_MAKE} test
+
+header "make distcheck"
+${MAKEFOO_MAKE} distcheck
+
